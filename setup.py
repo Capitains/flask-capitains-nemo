6f9e634903f57cd3a20f69d7e9df0dc0506b042d
Thibault Clerice <leponteineptique@gmail.com>
2015-10-20 14:54:04 +0200

    0.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='flask_nemo',
     version="0.0.1",
-    packages = find_packages(),
+    packages = find_packages(exclude=["examples"]),
     url='https://github.com/capitains/flask-capitains-nemo',
     license='GNU GPL',
     author='Thibault Clerice',
