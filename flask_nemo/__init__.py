bb00f2152a0e18b49376450f9cfb0224a8623417
Thibault Clérice <leponteineptique@gmail.com>
2016-02-05 11:44:04 +0100

    Added a new grouper which respects levels ending

--- flask_nemo/__init__.py
+++ flask_nemo/__init__.py
@@ -22,7 +22,7 @@ from copy import copy
 from pkg_resources import resource_filename
 from functools import reduce
 import flask_nemo._data
-from collections import defaultdict
+from collections import defaultdict, OrderedDict
 
 
 class Nemo(object):
@@ -988,6 +988,41 @@ class Nemo(object):
         return [(ref.split(":")[-1], ref.split(":")[-1]) for ref in references]
 
     @staticmethod
+    def level_grouper(text, getValidReff, level=None, groupby=20):
+        """ Alternative to level_chunker: groups levels together at the latest level
+
+        :param text: Text object
+        :param getValidReff: GetValidReff query callback
+        :param level: Level of citation to retrieve
+        :param groupby: Number of level to groupby
+        :return: Automatically curated references
+        """
+        if level is None or level > len(text.citation):
+            level = len(text.citation)
+
+        references = [ref.split(":")[-1] for ref in getValidReff(level=level)]
+        _refs = OrderedDict()
+
+        for key in references:
+            k = ".".join(key.split(".")[:level-1])
+            if k not in _refs:
+                _refs[k] = []
+            _refs[k].append(key)
+            del k
+
+        return [
+            (
+                _join_or_single(ref[0], ref[-1]),
+                _join_or_single(ref[0], ref[-1])
+            )
+            for sublist in _refs.values()
+            for ref in [
+                sublist[i:i+groupby]
+                for i in range(0, len(sublist), groupby)
+            ]
+        ]
+
+    @staticmethod
     def default_prevnext(passage, callback):
         """ Default deliver of prevnext informations
 
@@ -1081,3 +1116,19 @@ def _create_hierarchy(hierarchy, level):
     if level not in hierarchy:
         hierarchy[level] = OrderedDict()
     return hierarchy[level]
+
+
+def _join_or_single(start, end):
+    """
+
+    :param start:
+    :param end:
+    :return:
+    """
+    if start == end:
+        return start
+    else:
+        return "{}-{}".format(
+            start,
+            end
+        )
