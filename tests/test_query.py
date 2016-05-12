30dd43aec4508c29ea2dd109f0034815391124d7
Bridget Almas <balmas@gmail.com>
2016-05-12 17:03:54 -0400

    test Target

--- tests/test_query.py
+++ tests/test_query.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 from flask_nemo.query.proto import QueryPrototype, RetrieverPrototype
-from flask_nemo.query.annotation import Resolver
+from flask_nemo.query.annotation import Resolver, Target
 import MyCapytain.common.reference
 
 
@@ -10,11 +10,14 @@ class TestQuery(TestCase):
     def setUp(self):
        self.fakegetreffs = lambda x:x
        self.retrieverproto = RetrieverPrototype()
+       self.resolver = Resolver(self.retrieverproto)
+       self.fakeurn = MyCapytain.common.reference.URN("urn:cts:greekLit:tlg0012.tlg001.perseus-grc1")
+       self.faketarget = Target(self.fakeurn)
       
 
     def test_query_prototype(self):
         """ Test that the query prototype returns nothing """
-        self.assertEqual((0,[]), QueryPrototype(self.fakegetreffs).getAnnotations(MyCapytain.common.reference.URN("urn:cts:greekLit:tlg0012.tlg001.perseus-grc1")))
+        self.assertEqual((0,[]), QueryPrototype(self.fakegetreffs).getAnnotations(self.fakeurn))
 
     def test_retriever_prototype_read(self):
         """ Test that the retriever prototype reads nothing """
@@ -26,5 +29,8 @@ class TestQuery(TestCase):
 
     def test_resolver(self):
         """ Test that the default resolver, configured with retriever prototype, raises an exception on resolve """
-        self.assertRaises(Exception,Resolver(self.retrieverproto).resolve,"http://example.org")
+        self.assertRaises(Exception,self.resolver.resolve,"http://example.org")
         
+    def test_target_urn(self):
+        """ Test that a target returns its urn property """
+        self.assertEqual(self.fakeurn,self.faketarget.urn)
