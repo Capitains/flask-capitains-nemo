aa7ca73cf5be61301bdf0196dfe72413e9c91ce6
Bridget Almas <balmas@gmail.com>
2016-05-12 16:37:14 -0400

    test retriever move resolver to annotations

--- flask_nemo/query/annotation.py
+++ flask_nemo/query/annotation.py
@@ -66,3 +66,26 @@ class Target(object):
     def urn(self)
         return self.__urn__
      
+class Resolver(object):
+
+    """ Prototype for a Resolver
+    :param retriever: Retriever(s) to use to resolve resources passed to this resolver
+    :type retriever: Retriever
+    """
+    
+
+    def __init__(self,*retrievers,**kwargs):
+        self.__retrievers__  = retrievers
+
+
+    def resolve(self,uri):
+        """ Resolve a Resource identified by URI
+        :param uri: The URI of the resource to be resolved
+        :type uri: str
+        :return: the contents of the resource as a string
+        :rtype: str
+        """
+        for r in self.__retrievers__:
+            if r.canRetrieve(uri):
+                return r.read(uri)
+        raise Exception             
--- tests/test_query.py
+++ tests/test_query.py
@@ -1,16 +1,25 @@
 from unittest import TestCase
-from flask_nemo.query.proto import QueryPrototype
+from flask_nemo.query.proto import QueryPrototype, RetrieverPrototype
 import MyCapytain.common.reference
 
 
 class TestQuery(TestCase):
     """ Test query
     """
-
     def setUp(self):
-        self.query_api = QueryPrototype        
+       self.fakegetreffs = lambda x:x
+      
 
-    def test_prototype(self):
-        """ Test that prototype returns nothing """
-        self.assertEqual((0,[]), self.query_api.getAnnotations(MyCapytain.common.reference.URN("urn:cts:greekLit:tlg0012.tlg001.perseus-grc1")))
+    def test_query_prototype(self):
+        """ Test that the query prototype returns nothing """
+        self.assertEqual((0,[]), QueryPrototype(self.fakegetreffs).getAnnotations(MyCapytain.common.reference.URN("urn:cts:greekLit:tlg0012.tlg001.perseus-grc1")))
+
+    def test_retriever_prototype_read(self):
+        """ Test that the retriever prototype reads nothing """
+        self.assertEqual(None, RetrieverPrototype().read("http://example.org"))
+
+    def test_retriever_prototype_match(self):
+        """ Test that the retriever prototype matches nothing """
+        self.assertEqual(False, RetrieverPrototype.match("http://example.org"))
+
         
